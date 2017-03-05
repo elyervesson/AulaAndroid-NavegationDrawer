eb10f36c0f9b8c91c55047c7e040aec6b2a176c3
Elyervesson de Souza <Elyervesson de Souza>
2017-03-05 11:10:57 -0300

    Tutorial basico sobre navegation drawer

---
+++ app/src/main/java/com/elyer/navegationdrawertutorial/FirstFragment.java
@@ -0,0 +1,22 @@
+package com.elyer.navegationdrawertutorial;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/* alt + inset */
+
+public class FirstFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.first_layout, container, false);
+        return myView;
+    }
+}
---
+++ app/src/main/java/com/elyer/navegationdrawertutorial/SecondFragment.java
@@ -0,0 +1,22 @@
+package com.elyer.navegationdrawertutorial;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/* alt + inset */
+
+public class SecondFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.second_layout, container, false);
+        return myView;
+    }
+}
